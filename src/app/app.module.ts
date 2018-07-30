0b6fa06b43d8f686fe6eeac3611b26237ccf89d0
Khiem Le <LeK@deea.thebuffalogroup.com>
2018-07-30 09:37:36 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule
